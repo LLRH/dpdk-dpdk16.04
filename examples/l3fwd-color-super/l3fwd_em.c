f34b6e7f4872e69bb3157ac7d7a7142578d642b7
1016894282@qq.com <wenxingbeng>
2017-11-21 12:09:26 +0800

    fix bug

--- examples/l3fwd-color-super/l3fwd_em.c
+++ examples/l3fwd-color-super/l3fwd_em.c
@@ -662,7 +662,6 @@ insert_mongodb (control_register_t *control_register_hdr)
                         *insert;
    bson_error_t          error;
    char                 *str;
-   bool                  retval;
 
 
 //------
@@ -703,7 +702,6 @@ insert_mongodb (control_register_t *control_register_hdr)
    }
 
    bson_destroy (insert);
-   bson_destroy (&reply);
    bson_destroy (command);
    bson_free (str);
    return 0;
@@ -712,49 +710,10 @@ insert_mongodb (control_register_t *control_register_hdr)
 
 int find_mongodb (CoLoR_get_t *get_hdr)
 {
-   mongoc_client_t      *client;
-   mongoc_database_t    *database;
-   mongoc_collection_t  *collection;
    bson_t               *command,
                          reply;
    bson_error_t          error;
    char                 *str;
-   bool                  retval;
-
-   /*
-    * Required to initialize libmongoc's internals
-    */
-   mongoc_init ();
-
-   /*
-    * Create a new client instance
-    */
-   client = mongoc_client_new ("mongodb://localhost:27017");
-
-   /*
-    * Register the application name so we can track it in the profile logs
-    * on the server. This can also be done from the URI (see other examples).
-    */
-   mongoc_client_set_appname (client, "connect-example");
-
-   /*
-    * Get a handle on the database "db_name" and collection "coll_name"
-    */
-   database = mongoc_client_get_database (client, DB_NAME);
-   collection = mongoc_client_get_collection (client, DB_NAME, COLL_NAME);
-
-   /*
-    * Do work. This example pings the database, prints the result as JSON and
-    * performs an insert
-    */
-   command = BCON_NEW ("ping", BCON_INT32 (1));
-
-   retval = mongoc_client_command_simple (client, "admin", command, NULL, &reply, &error);
-
-   if (!retval) {
-      fprintf (stderr, "%s\n", error.message);
-      return EXIT_FAILURE;
-   }
 
    str = bson_as_json (&reply, NULL);
    printf ("%s\n", str);
@@ -787,18 +746,8 @@ int find_mongodb (CoLoR_get_t *get_hdr)
     }
 
    bson_destroy (query);
-   bson_destroy (&reply);
    bson_destroy (command);
    bson_free (str);
-
-   /*
-    * Release our handles and clean up libmongoc
-    */
-   mongoc_collection_destroy (collection);
-   mongoc_database_destroy (database);
-   mongoc_client_destroy (client);
-   mongoc_cleanup ();
-
    return 0;
 }
 
