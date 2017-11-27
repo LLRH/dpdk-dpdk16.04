b7a13236080f6cc9ed7960ec0fbff87b856e05c1
wenxingbeng_i <wenxingbeng_i@didichuxing.com>
2017-11-27 15:53:39 +0800

    增加查询结束打印

--- examples/l3fwd-color-super/l3fwd_em.c
+++ examples/l3fwd-color-super/l3fwd_em.c
@@ -963,7 +963,7 @@ em_get_dst_port_pumpking(const struct lcore_conf *qconf, struct rte_mbuf *pkt,ui
 				
 			CoLoR_get_t * get_hdr=(CoLoR_get_t *)(ipv4_hdr+1);
 			find_mongodb (get_hdr);
-
+            printf("[From %s]find_mongodb over\n",__func__);
             //TODO:测试删除的功能，查找后并删除这个记录
 
 		}
