51efd48a82980bae0f83bd6415dfd8e6bfee33a2
wenxingbeng_i <wenxingbeng_i@didichuxing.com>
2017-12-27 22:37:36 +0800

    增加进制转换

--- examples/l3fwd-color-super/l3fwd_em.c
+++ examples/l3fwd-color-super/l3fwd_em.c
@@ -791,6 +791,28 @@ struct sid_port_route{
     ValType val_port;
 };
 
+//大写0123456789ABCDEF
+uint8_t HEX2Ten(char c){
+    switch(c){
+        case '0': return 0;
+        case '1': return 1;
+        case '2': return 2;
+        case '3': return 3;
+        case '4': return 4;
+        case '5': return 5;
+        case '6': return 6;
+        case '7': return 7;
+        case '8': return 8;
+        case '9': return 9;
+        case 'A': return 10;
+        case 'B': return 11;
+        case 'C': return 12;
+        case 'D': return 13;
+        case 'E': return 14;
+        case 'F': return 15;
+    }
+    return 0;
+}
 
 void convert_str_2_sid_port_route(char *value_str, struct sid_port_route item){
     puts(value_str);
@@ -805,6 +827,7 @@ void convert_str_2_sid_port_route(char *value_str, struct sid_port_route item){
     }
     for(i=0;i<L_SID_LENGTH;i++){
         printf("[%c%c]",value_str[2*i+2],value_str[2*i+1+2]);
+        printf("[%u]",HEX2Ten(value_str[2*i+2])*HEX2Ten(value_str[2*i+1+2]));
     }
     printf("\n");
 };
