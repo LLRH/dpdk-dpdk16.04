a8faf55f323754f6067070a5e3a151545e3df07d
wenxingbeng_i <wenxingbeng_i@didichuxing.com>
2018-03-04 10:31:30 +0800

    zzz

--- examples/l3fwd-color-super/l3fwd_em.c
+++ examples/l3fwd-color-super/l3fwd_em.c
@@ -1014,10 +1014,10 @@ em_get_dst_port_pumpking(const struct lcore_conf *qconf, struct rte_mbuf *pkt,ui
 
 	//TODO:SID在另外一个Socket的那个表上，当时有两个Socket,现在只有一个
 	if(/*next_hop==255*/ 1){
-        uint64_t cur_tsc1 = rte_rdtsc();
+        uint64_t cur_tsc11 = rte_rdtsc();
         res=cuckoo_find_bulk_batch( qconf->sid_lookup_struct_another_socket,&key_array[0] , 1,&next_hop );
-        uint64_t cur_tsc2 = rte_rdtsc();
-        printf("hz_timer=%"PRIu64" [Remote] %"PRIu64" \n",hz_timer,cur_tsc2-cur_tsc1);
+        uint64_t cur_tsc22 = rte_rdtsc();
+        printf("hz_timer=%"PRIu64" [Remote] %"PRIu64" \n",hz_timer,cur_tsc22-cur_tsc11);
     }
 
 
