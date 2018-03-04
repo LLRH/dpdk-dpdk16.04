6701d2ecbe3ac731418af204f2de6e3bd11fc8cc
wenxingbeng_i <wenxingbeng_i@didichuxing.com>
2018-03-04 10:55:53 +0800

    fff

--- examples/l3fwd-color-super/l3fwd_em.c
+++ examples/l3fwd-color-super/l3fwd_em.c
@@ -1008,7 +1008,7 @@ em_get_dst_port_pumpking(const struct lcore_conf *qconf, struct rte_mbuf *pkt,ui
 
     //uint64_t hz_timer = rte_get_timer_hz();
     //uint64_t cur_tsc1 = rte_rdtsc();
-	//res=cuckoo_find_bulk_batch( qconf->sid_lookup_struct,&key_array[0] , 1,&next_hop );
+	res=cuckoo_find_bulk_batch( qconf->sid_lookup_struct,&key_array[0] , 1,&next_hop );
     //uint64_t cur_tsc2 = rte_rdtsc();
     //printf("hz_timer=%"PRIu64" [Local] %"PRIu64" \n",hz_timer,cur_tsc2-cur_tsc1);
 
