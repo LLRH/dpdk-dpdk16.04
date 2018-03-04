366272502d9a29632e8c729272171f7363afa790
wenxingbeng_i <wenxingbeng_i@didichuxing.com>
2018-03-04 10:04:14 +0800

    xxx

--- examples/l3fwd-color-super/l3fwd_em.c
+++ examples/l3fwd-color-super/l3fwd_em.c
@@ -1008,7 +1008,7 @@ em_get_dst_port_pumpking(const struct lcore_conf *qconf, struct rte_mbuf *pkt,ui
 
     uint64_t hz_timer = rte_get_timer_hz();
 	res=cuckoo_find_bulk_batch( qconf->sid_lookup_struct,&key_array[0] , 1,&next_hop );
-    printf("hz_timer=%PRIu64\n",hz_timer);
+    printf("hz_timer=%"PRIu64"\n",hz_timer);
 
 	//TODO:SID在另外一个Socket的那个表上，当时有两个Socket,现在只有一个
 	if(next_hop==255)
